535d20bcf22c2874edeb0052b700f0ae65ef2524
Jason Erdmann <jason.erdmann@tradingtechnologies.com>
2014-09-23 09:35:43 -0500

    custom

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -50,3 +50,15 @@
 (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
 
 (put 'downcase-region 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("0e121ff9bef6937edad8dfcff7d88ac9219b5b4f1570fd1702e546a80dba0832" "bd115791a5ac6058164193164fd1245ac9dc97207783eae036f0bfc9ad9670e0" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
