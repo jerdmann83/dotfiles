46cbd4998d5b208da58eb28e4fca6ba8685539ab
Jason Erdmann <jason.erdmann@tradingtechnologies.com>
2014-09-25 10:33:37 -0500

    not smart enough for multiple files yet

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -41,11 +41,6 @@
 (setq mouse-wheel-follow-mouse 't)
 (setq scroll-step 1)
 
-(global-set-key (kbd "<RET>") 'newline-and-indent)
-(global-set-key (kbd "C-x k") 'kill-this-buffer)
-(global-set-key (kbd "C-a") 'beginning-of-line-text)
-(global-set-key (kbd "C-c C-c") 'my/reindent-copy-whole-buffer)
-
 (setq tramp-default-method "ssh")
 
 (add-hook 'after-init-hook (load-theme 'wombat t))
@@ -53,3 +48,43 @@
 
 (put 'downcase-region 'disabled nil)
 
+;function definitions
+(defun my/reindent-buffer ()
+  "Reindent entire buffer."
+  (interactive)
+  (save-excursion
+    (mark-whole-buffer)
+    (indent-region (region-beginning) (region-end))))
+
+(defun my/replace-string-whole-buffer ()
+  "Whole buffer version of replace-string."
+  (interactive)
+  (save-excursion
+    (beginning-of-buffer)
+    (call-interactively 'replace-string)))
+
+(defun my/replace-regexp-whole-buffer ()
+  "Whole buffer version of replace-regexp."
+  (interactive)
+  (save-excursion
+    (beginning-of-buffer)
+    (call-interactively 'replace-regexp)))
+
+(defun my/copy-whole-buffer ()
+  "Copy the entire buffer to the clipboard."
+  (interactive)
+  (save-excursion
+    (mark-whole-buffer)
+    (copy-region-as-kill (region-beginning) (region-end))))
+
+(defun my/reindent-copy-whole-buffer ()
+  "Reindent and copy the whole buffer to the clipboard."
+  (interactive)
+  (my/reindent-buffer)
+  (my/copy-whole-buffer))
+
+;key bindings
+(global-set-key (kbd "<RET>") 'newline-and-indent)
+(global-set-key (kbd "C-x k") 'kill-this-buffer)
+(global-set-key (kbd "C-a") 'beginning-of-line-text)
+(global-set-key (kbd "C-c C-c") 'my/reindent-copy-whole-buffer)
