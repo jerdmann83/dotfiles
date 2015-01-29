ca1d84b60f8d15360a3ab009f4b8a8d3024f5a79
Jason Erdmann <jason.erdmann@tradingtechnologies.com>
2015-01-28 20:00:40 -0600

    fucking done with emacs, all hail vim the one true editor

--- .emacs.d/ruby.el
+++
@@ -1,6 +0,0 @@
-(defun my/toggle-ruby-indent ()
-  "Completely non-working ruby indent toggle."
-  (interactive)
-  (setq ruby-indent-level (if (= ruby-indent-level 2) 4 2))
-  (message (format "Set ruby indent level to %d." ruby-indent-level))
-  )
